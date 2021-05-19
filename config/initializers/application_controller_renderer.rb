f33f1bc0906ad7fee9cf8a873b949fd668170139
akahito1006 <nekodora5k@gmail.com>
2021-05-19 14:52:45 +0000

    single upload

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2a5727b2f87967e796559c625c2623dc81cced9e472352000e0b3870ea27674884f9730ff88bfdd6004a2babbb085159cd0b9245d50b3b969bc52a4416f02a47'
